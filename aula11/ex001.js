0bedaef623d74b9601e8ce27de97e7a393f56e2e
Nattanjunior <fnatanieljunior@gmail.com>
2022-12-22 03:18:32 -0300

    Condições em JavaScript

---
+++ aula11/ex001.js
@@ -0,0 +1,9 @@
+var velocidade = 60
+
+console.log(`A velocidade do seu carro é  ${velocidade} km/h`)
+
+if (velocidade > 60) { //Condição Simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
