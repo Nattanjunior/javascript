28cce179e3681a779932166460f1dbfbe30822cc
Nattanjunior <fnatanieljunior@gmail.com>
2022-12-28 16:09:21 -0300

    aula17ex02 em JavaScript

--- aula17ex02/script.js
+++ aula17ex02/script.js
@@ -21,8 +21,42 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(n.value) && !inLista(n.value, valores)) {
-        alert('Tudo ok!')
+       valores.push(Number(n.value))
+       let item = document.createElement('option')
+       item.text = `Valor ${n.value} adicionado.`
+       lista.appendChild(item)
+       res.innerHTML = ''
     }else{
         alert('Valor inválido ou já encontrado na lista.')
     }
+    n.value = ''
+    n.focus()
+}
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+         res.innerHTML = ''
+         res.innerHTML+= `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+         res.innerHTML += `<p> O Maior valor informado foi ${maior}.</p>`
+         res.innerHTML += `O Menor valor informado foi ${menor}.`
+         res.innerHTML += `<p> Somando todos os valores, temos ${soma}</p>`
+         res.innerHTML += `<p>A media dos valores digitados é ${media}</p>`
+    }
+    
+
 }
